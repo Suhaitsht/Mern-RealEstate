3e7ddf3eb5bbce971d7ff6069fbdf9b1e9e219d9
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-04-02 13:45:38 +0530

    Create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,39 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignOut />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
 
 export default function App() {
-  return <div className="text-red-400">App</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
