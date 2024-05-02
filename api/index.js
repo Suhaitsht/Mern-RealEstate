0814a7de8147fc5c76aaaec58896d0168bcb93c2
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-05-02 13:27:42 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONOGODB)
+  .then(() => {
+    console.log("Connected to Mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running port is 300!");
