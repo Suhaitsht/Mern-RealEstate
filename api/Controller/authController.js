a1a924a67489f43692ba254e743f9a65b0444722
Suhaitsht <nm.suhaitsuhait@gmail.com>
2024-05-03 22:41:09 +0530

    create a middleware and function to handle possible error

--- api/Controller/authController.js
+++ api/Controller/authController.js
@@ -1,15 +1,17 @@
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 
-exports.userSignup = async (req, res) => {
+exports.userSignup = async (req, res, next) => {
   const { username, password, email } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 16);
-  const newUser = await User.create({
-    username,
-    password: hashPassword,
-    email,
-  });
-  res.status(200).json({
-    user: newUser,
-  });
+  try {
+    const newUser = await User.create({
+      username,
+      password: hashPassword,
+      email,
+    });
+    res.status(200).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.use("/api/auth", AuthRouter);
 app.listen(3000, () => {
   console.log("Server is running port is 3000!");
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
